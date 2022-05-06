5e1bddc30a520d2f52fb03201f884e442e4521c2
student <student@localhost.localdomain>
2022-05-06 00:40:16 -0700

    minor yhd changes

--- index.php
+++ index.php
@@ -6,11 +6,8 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+		<p><b>An NEW YHD Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>DZJEN DOBRE GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
 	</body> 
 </html>
